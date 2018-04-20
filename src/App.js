fedc24d0ad7865af9f160491dc17bfbd9c72b8f5
Wei Xu <stephenweixu@gmail.com>
2018-04-19 22:46:42 -0400

    delete image and try some react code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class SwitchButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isButtonON: true};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.setState( prevState => ({
+      isButtonON: !prevState.isButtonON
+    }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to this site</h1>
-        </header>
-        <p className="App-intro">
-          This is a demo site. It gonna start with demo right?.
-        </p>
+      <button onClick={this.handleClick}>I am {this.state.isButtonON ? 'ON' : 'OFF'}</button>
+    );
+  }
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval( () => this.tick(), 1000);
+  }
+
+  componentWillMount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({date: new Date()});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Now is { this.state.date.toLocaleTimeString() }.</h1>
       </div>
     );
   }
 }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Clock />
+        <SwitchButton />
+      </div>
+    );
+  }
+}
+
 
 export default App;
