740709be457f67d0d208f4a8d1dbb61ff7fb6a9a
Wei Xu <stephenweixu@gmail.com>
2018-04-25 21:22:38 -0400

    foudation sites import working now

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Route, NavLink, HashRouter } from "react-router-dom";
 import $ from "jquery";
-import "foundation-sites/dist/css/foundation.css";
-import "foundation-sites/dist/js/foundation.min.js";
 import Home from './components/Home';
 import Project from './components/Project';
 import Publication from './components/Publication';
@@ -17,27 +15,27 @@ class App extends Component {
 	}
 
 	render() {
-	return(
-		<HashRouter>
-			<div>
-				<div className="top-bar">
-					<div className="top-bar-left">
-						<ul className="dropdown menu" data-dropdown-menu>
-							<li> <NavLink to="/">HOME</NavLink></li>
-							<li> <NavLink to="/project">Project</NavLink></li>
-							<li> <NavLink to="/publication">PUBLICATION</NavLink></li>
-						</ul>
+		return(
+			<HashRouter>
+				<div>
+					<div className="top-bar">
+						<div className="top-bar-left">
+							<ul className="dropdown menu" data-dropdown-menu>
+								<li> <NavLink to="/">HOME</NavLink></li>
+								<li> <NavLink to="/project">PROJECT</NavLink></li>
+								<li> <NavLink to="/publication">PUBLICATION</NavLink></li>
+							</ul>
+						</div>
+					</div>
+
+					<div className="content">
+						<Route exact path="/" component={Home} />
+						<Route path="/project" component={Project} />
+						<Route path="/publication" component={Publication} />
 					</div>
 				</div>
-
-				<div className="content">
-					<Route exact path="/" component={Home} />
-					<Route path="/project" component={Project} />
-					<Route path="/publication" component={Publication} />
-				</div>
-			</div>
-		</HashRouter>
-	);
+			</HashRouter>
+		);
 	}
 }
 
