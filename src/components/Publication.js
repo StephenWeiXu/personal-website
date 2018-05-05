f3e9dbaf94d9b074a90f2c712964b00020b74569
Wei Xu <stephenweixu@gmail.com>
2018-05-05 10:29:32 -0400

    use accordion for publication text

--- src/components/Publication.js
+++ src/components/Publication.js
@@ -11,9 +11,11 @@ class Publication extends Component {
 					<span className="bold">Wei Xu</span>, Xuemeng Yang and Zhong Ji. Image classification with saliency region and multi-task learning. In <span className="italic">Proceedings of the Fifth International Conference on Internet Multimedia Computing and Service(ICIMCS'13)</span>, pp. 199-202, 2013.
 					<a className="mls" href="https://dl.acm.org/citation.cfm?id=2499811&dl=ACM&coll=DL" target="_blank">View</a>
 				</p>
-				<Description />
-				<Experiment />
-				<Conclusion />
+				<ul className="accordion" data-accordion data-multi-expand="true" data-allow-all-closed="true">
+					<Description />
+					<Experiment />
+					<Conclusion />
+				</ul>
 			</div>
 		)
 	}
