97abfc757d8a51dfcb77cab12e806fcef91833a2
Wei Xu <stephenweixu@gmail.com>
2018-09-03 10:31:56 -0400

    add index file and script for umich site redirect

---
+++ redirect/upload_file_to_umich.sh
@@ -0,0 +1,5 @@
+echo "Copy file to umich server..."
+scp index.html weixu@login.itd.umich.edu:/afs/umich.edu/user/w/e/weixu/Public/html/
+
+echo "Copy done..."
+echo "Check out the site to be successfully redirected at http://www-personal.umich.edu/~weixu/index.html"
