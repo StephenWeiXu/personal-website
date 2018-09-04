d96eea30bf55658f170aa96f13f7683a0d54994d
Wei Xu <stephenweixu@gmail.com>
2018-09-03 23:19:04 -0400

    update s3 file sync command

--- publish_site.sh
+++ publish_site.sh
@@ -2,7 +2,7 @@ echo "npm build latest assets files..."
 npm run build
 
 echo "Copy file to umich server..."
-aws s3 sync build/ s3://stephenweixu.com/ --recursive
+aws s3 sync build/ s3://stephenweixu.com/
 
 echo "Copy done..."
 echo "Check out the site at http://stephenweixu.com"
